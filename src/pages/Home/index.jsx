00cb320c9aecc83b2a8aaa3d75a9d364b3bd6746
JeisonYamarte <jeisonyamarte16@gmail.com>
2024-08-24 23:42:33 -0400

    Creacion de las Card desde la API de Fake Store Platzi

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,10 +3,28 @@ import { Layout } from '../../Components/Layout'
 import { Card } from '../../Components/Card'
 
 function Home() {
+  const API = 'https://api.escuelajs.co/api/v1/products';
+  const [items, setItems] = React.useState(null);
+
+  React.useEffect(()=>{
+    fetch(API)
+      .then(response => response.json())
+      .then(data => setItems(data));
+      
+  },[])
+
+
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+      {
+        items?.map((item)=>{
+          return <Card key={item.id} data={item} />
+        })
+      }
+      </div>
     </Layout>
   )
 }
